3b62892103ecb223faa6d6fe775ea25298d4f968
Ismail Elyan <ismailalyan1234@gmail.com>
2025-09-17 15:27:13 +0300

    Added caching

--- rag/retriever.py
+++ rag/retriever.py
@@ -7,20 +7,36 @@ from langchain_core.prompts import PromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 import streamlit as st
+import tempfile, shutil, atexit
+
 
 st.title("🩺 Cancer Information Chatbot")
 
-loader = PyPDFLoader("Understanding-Cancer-What-is-Cancer-and-Types-of-Cancer_rev.pdf")
-text = loader.load()
 
-text_split = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 20)
-documents = text_split.split_documents(text)
+@st.cache_resource
+def load_faiss():
+    loader = PyPDFLoader("Understanding-Cancer-What-is-Cancer-and-Types-of-Cancer_rev.pdf")
+    text = loader.load()
 
-db = FAISS.from_documents(documents,OllamaEmbeddings(model = 'nomic-embed-text'))
+    text_split = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    documents = text_split.split_documents(text)
 
+    # Temporary FAISS index
+    temp_dir = tempfile.mkdtemp()
+
+    # Cleanup on exit
+    atexit.register(lambda: shutil.rmtree(temp_dir, ignore_errors=True))
+
+    db = FAISS.from_documents(documents, OllamaEmbeddings(model="nomic-embed-text"))
+    db.save_local(temp_dir)
+
+    return db
+
+
+db = load_faiss()
 retriever = db.as_retriever()
 
-llm = ChatOllama(model='llama3.2')
+llm = ChatOllama(model="llama3.2")
 
 prompt = PromptTemplate(
     template=(
@@ -32,9 +48,9 @@ prompt = PromptTemplate(
     input_variables=["context", "input"],
 )
 
-documents_chain = create_stuff_documents_chain(llm,prompt)
+documents_chain = create_stuff_documents_chain(llm, prompt)
+retrieval_chain = create_retrieval_chain(retriever, documents_chain)
 
-retrieval_chain = create_retrieval_chain(retriever,documents_chain) # takes retriver and documents chain
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -46,7 +62,6 @@ for msg in st.session_state.messages:
 
 
 if user_input := st.chat_input("Ask about cancer..."):
-    
     st.session_state.messages.append({"role": "user", "content": user_input})
     with st.chat_message("user"):
         st.write(user_input)
